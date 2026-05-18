3c65e25b1a24f368fda91a57435ac3f8a29ba5d5
nathanjeonz <jeonz0102@gmail.com>
2026-05-18 19:22:31 +0700

    commit

--- step 1 Fundamentals of Python/Topic_1_DataTypes.py
+++ step 1 Fundamentals of Python/Topic_1_DataTypes.py
@@ -7,3 +7,40 @@ height = 1.75
 is_student = True
 print("Name:", name, "Age:", age, "Height:", height, "Is Student:", is_student)
 print(f"Name: {name}, Age: {age}, Height: {height}, Is Student: {is_student}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#http://youtube.com/watch?v=N14VV4UUA3I
